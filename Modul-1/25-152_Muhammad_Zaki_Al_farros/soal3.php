902c9e07d0d3dc5829fc5ed25c99abeced65f2d9
Muhammad Zaki Al Farros <muhammad.zaki100186@sma.belajar.id>
2026-09-25 20:37:16 +0700

    Tugas Praktikum - Modul 1

---
+++ Modul-1/25-152_Muhammad_Zaki_Al_farros/soal3.php
@@ -0,0 +1,19 @@
+<?php
+// ini non embedded script
+echo "Hello World";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php
+	// ini embedded script
+	echo "Hello World";
+	?>
+</body>
+</html>
